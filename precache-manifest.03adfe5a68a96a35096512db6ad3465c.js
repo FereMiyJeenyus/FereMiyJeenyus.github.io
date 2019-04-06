142ca0ec90b65c31a5e4071894c30661f9e36d9f
FereMiyJeenyus <feremiyjeenyus@gmail.com>
2019-04-06 09:46:53 -0500

    Updates

---
+++ precache-manifest.03adfe5a68a96a35096512db6ad3465c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b087319539f0d7065173",
+    "url": "/static/js/main.bda55bc4.chunk.js"
+  },
+  {
+    "revision": "4b9162f6d4a3a422b3a0",
+    "url": "/static/js/2.aafb9067.chunk.js"
+  },
+  {
+    "revision": "b087319539f0d7065173",
+    "url": "/static/css/main.ed223925.chunk.css"
+  },
+  {
+    "revision": "02070081453b0400ca8f1773d9bffb7a",
+    "url": "/index.html"
+  }
+];
